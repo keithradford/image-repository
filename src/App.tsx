0d7fc9a586651a19f0f3fff8a2e7934c207e851a
keith <khr8771@gmail.com>
2021-09-23 15:16:16 -0700

    feat: initialize unsplash api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createApi } from "unsplash-js";
+import { Fragment, useEffect, useState } from "react";
+
+require("dotenv").config({ path: __dirname + "/.env" });
+
+// on your node server
+const api = createApi({
+  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY as string,
+});
+
+type Photo = {
+  id: number;
+  width: number;
+  height: number;
+  urls: { large: string; regular: string; raw: string; small: string };
+  color: string | null;
+  user: {
+    username: string;
+    name: string;
+  };
+};
+
+const PhotoComp: React.FC<{ photo: Photo }> = ({ photo }) => {
+  const { user, urls } = photo;
+
+  return (
+    <Fragment>
+      <img className="img" src={urls.regular} />
+      <a
+        className="credit"
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        {user.name}
+      </a>
+    </Fragment>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setPhotosResponse] = useState<any>(null);
+
+  console.log(process.env.UNSPLASH_ACCESS_KEY);
+  console.log(require("dotenv").config());
+  useEffect(() => {
+    api.search
+      .getPhotos({ query: "cat", orientation: "landscape" })
+      .then((result) => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }, []);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="feed">
+        <ul className="columnUl">
+          {data.response.results.map((photo: Photo) => (
+            <li key={photo.id} className="li">
+              <PhotoComp photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
